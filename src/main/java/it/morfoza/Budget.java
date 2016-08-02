cd0a177a5eabdb76608d6a8ed588dd5c10f4d3f6
ahannke <ahannke@gmail.com>
2016-08-02 17:59:36 +0200

    Budget project created

---
+++ src/main/java/it/morfoza/Budget.java
@@ -0,0 +1,14 @@
+package it.morfoza;
+
+/**
+ * Created by ahann on 02/08/2016.
+ */
+public class Budget {
+
+    public static void main(String[] args) {
+        System.out.println("Ile masz hajsu na wakacje?");
+        System.out.println("Na ile dni chcesz wyjechac?");
+
+    }
+
+}
---
+++ src/main/java/it/morfoza/Destination.java
@@ -0,0 +1,7 @@
+package it.morfoza;
+
+/**
+ * Created by ahann on 02/08/2016.
+ */
+public class Destination {
+}
