51ad2ceca383ef7aff7df2bb75935383f0713e48
ahannke <ahannke@gmail.com>
2016-08-02 18:26:04 +0200

    daily budget successfully counted

--- src/main/java/it/morfoza/Budget.java
+++ src/main/java/it/morfoza/Budget.java
@@ -1,13 +1,34 @@
 package it.morfoza;
 
+import java.util.Scanner;
+
 /**
  * Created by ahann on 02/08/2016.
  */
 public class Budget {
 
+
     public static void main(String[] args) {
-        System.out.println("Ile masz hajsu na wakacje?");
-        System.out.println("Na ile dni chcesz wyjechac?");
+        Scanner input = new Scanner(System.in);
+
+        double number1;
+
+        double number2;
+
+
+        System.out.println("Ile masz hajsu na wakacje? (podaj w PLN)");
+        number1 = input.nextInt();
+
+        System.out.println("Na ile dni chcesz wyjechać?");
+        number2 = input.nextInt();
+
+
+        double dailyBudget = number1/number2;
+        System.out.println("Twój dzienny budżet to: " + dailyBudget);
+
+
+
+
 
     }
 
